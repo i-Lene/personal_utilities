7308d8f91076a6b971d0608ba36181d0ad33e37e
i-Lene <marlene.s.veiga@gmail.com>
2025-06-05 17:39:46 +0100

    done in progress

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router";
+import homeImg from "/images/home.gif";
+import "./Home.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
+      <div className="text-wrapper">
+        <h1>Welcome to the Utils App</h1>
+        <div className="actions">
+          <Link className="home-btn" to="/todo">
+            Go to Todo List  <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+          <Link className="home-btn" to="/blog">
+            Go to Blog <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+          <Link className="home-btn" to="/weather">
+            Go to Weather Tab  <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+        </div>
+      </div>
+      <div className="img-wrapper">
+        <img src={homeImg} alt="Home" />
+      </div>
     </div>
   );
 }
